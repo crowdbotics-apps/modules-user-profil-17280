298b3a6536095c4e828bff9d68e31b5d3d3c851e
Crowdbotics <team@crowdbotics.com>
2020-12-24 17:41:06 +0000

    Update models

--- backend/modules_user_profil_17280/wsgi.py
+++ backend/modules_user_profil_17280/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modules_user_profil_17280.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "modules_user_profil_17280.settings")
 
 application = get_wsgi_application()
